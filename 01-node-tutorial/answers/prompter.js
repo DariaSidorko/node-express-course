27415015d85db307fedcba84163eb074439acae5
DariaSidorko <daria.sidorko@gmail.com>
2025-03-21 22:19:52 -0400

    answers for lesson 2

--- 01-node-tutorial/answers/prompter.js
+++ 01-node-tutorial/answers/prompter.js
@@ -59,5 +59,10 @@ const server = http.createServer((req, res) => {
   }
 });
 
+
+server.on("request", (req) => {  
+  console.log("event received: ", req.method, req.url);  
+});  
+
 server.listen(3000);
 console.log("Server is listening on port 3000");
---
+++ 01-node-tutorial/answers/writeWithPromisesAwait.js
@@ -0,0 +1,29 @@
+
+
+const { writeFile, readFile } = require("fs").promises;  
+
+async function writer() {
+    try {
+        await writeFile("temp.txt", "First line\n");
+        await writeFile("temp.txt", "Second line\n", { flag: "a" });
+        await writeFile("temp.txt", "Third line\n", { flag: "a" });
+      } catch (error) {
+        console.error("Error writing file:", error);
+    }
+}
+
+async function reader() {
+    try {
+      const data = await readFile("temp.txt", "utf8");
+      console.log("File contents:\n", data);
+    } catch (error) {
+      console.error("Error reading file:", error);
+    }
+  }
+  
+  async function readWrite() {
+    await writer();
+    await reader();
+  }
+  
+  readWrite();
