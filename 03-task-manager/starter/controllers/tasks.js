dfb26f829fc90b34ea5d1e5b8de67c0bd2680f28
DariaSidorko <daria.sidorko@gmail.com>
2025-04-15 20:10:30 -0400

    task manager base model, controllers, routes set

---
+++ 03-task-manager/starter/controllers/tasks.js
@@ -0,0 +1,34 @@
+
+const Task = require('../models/Task')
+
+const getAllTasks =  (req, res) => {
+  res.send('all items')
+}
+
+const createTask = async (req, res) => {
+    const task = await Task.create(req.body)
+    res.status(201).json({ task })
+  }
+
+const getTask =  (req, res) => {
+    res.send('get ones')
+}
+
+const deleteTask =  (req, res) => {
+    res.send('delete')
+}
+
+const updateTask =  (req, res) => {
+    res.send('update')
+}
+
+  
+ module.exports = {
+    getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask,
+  }
+  
+
