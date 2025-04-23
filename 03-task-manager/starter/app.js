5a8560fff672a47b12d8cc867d6ff171523f3cb1
DariaSidorko <daria.sidorko@gmail.com>
2025-04-22 22:40:36 -0400

    assignment6, finishing schema, custom error handlers, refuctoring

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -4,16 +4,20 @@ const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
 
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
 // middleware
 
 app.use(express.static('./public'));
+app.use(express.json());
 
 // routes
-app.get('/hello', (req, res) => {
-    res.send('Task Manager App')
-})
 
 app.use('/api/v1/tasks', tasks);
+
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 3000;
 
